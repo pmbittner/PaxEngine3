2e1bdb13f9ea951022e4f04bfafde1825d5a7a62
bittner <p.bittner@tu-bs.de>
2017-11-20 17:12:16 +0100

    files for terrain generation

---
+++ include/generation/terrain/Terrain.h
@@ -0,0 +1,12 @@
+//
+// Created by bittner on 11/20/17.
+//
+
+#ifndef PAXENGINE3_TERRAIN_H
+#define PAXENGINE3_TERRAIN_H
+
+namespace PAX {
+
+}
+
+#endif //PAXENGINE3_TERRAIN_H
---
+++ include/generation/terrain/TerrainGenerator.h
@@ -0,0 +1,15 @@
+//
+// Created by bittner on 11/20/17.
+//
+
+#ifndef PAXENGINE3_TERRAINGENERATOR_H
+#define PAXENGINE3_TERRAINGENERATOR_H
+
+namespace PAX {
+    class TerrainGenerator {
+    public:
+
+    };
+}
+
+#endif //PAXENGINE3_TERRAINGENERATOR_H
