744418d17cbf306cb6dcd8a96f5cb8ff8f3e0af7
Paul Maximilian Bittner <p.bittner@tu-bs.de>
2017-12-21 22:03:31 +0100

    Tuple Printer, conversion of template types to string

--- include/utility/reflection/TemplateTypeToString.h
+++ include/utility/reflection/TemplateTypeToString.h
@@ -5,11 +5,17 @@
 #ifndef PAXENGINE3_TEMPLATETYPETOSTRING_H
 #define PAXENGINE3_TEMPLATETYPETOSTRING_H
 
-#include <memory.h>
 #include <utility/macros/CompilerDetection.h>
+
+#ifdef PAX_COMPILER_GCC
+#include <typeinfo>
+#else
+#include <memory.h>
+#endif
 
 namespace PAX {
     namespace Reflection {
+#if defined(PAX_COMPILER_MSVC) || defined(PAX_COMPILER_CLANG)
         /**
          * This code is copied from https://blog.molecular-matters.com/2015/12/11/getting-the-type-of-a-template-argument-as-string-without-rtti/
          * at 2017 12 19
@@ -17,47 +23,29 @@ namespace PAX {
          * Adapted to GCC
          */
         namespace internal {
-            static const unsigned int FRONT_SIZE =
-    #ifdef PAX_COMPILER_GCC
-                    sizeof("internal::GetTypeNameHelper<") - 1u;
-    #else
-                    sizeof("internal::GetTypeNameHelper<") - 1u;
-    #endif
-            static const unsigned int BACK_SIZE =
-    #ifdef PAX_COMPILER_GCC
-                    sizeof(">::GetTypeName") - 1u;
-    #else
-                    sizeof(">::GetTypeName") - 1u;
-    #endif
+            static const unsigned int FRONT_SIZE = sizeof("internal::GetTypeNameHelper<") - 1u;
+            static const unsigned int BACK_SIZE = sizeof(">::GetTypeName") - 1u;
 
             template<typename T>
             struct GetTypeNameHelper {
                 static const char *GetTypeName(void) {
-                    static const size_t size =
-    #ifdef PAX_COMPILER_GCC
-                            sizeof(__FUNCTION__)
-    #else
-                            sizeof(__FUNCTION__)
-    #endif
-                            - FRONT_SIZE - BACK_SIZE;
+                    static const size_t size = sizeof(__FUNCTION__) - FRONT_SIZE - BACK_SIZE;
                     static char typeName[size] = {};
-                    memcpy(typeName,
-    #ifdef PAX_COMPILER_GCC
-                           __FUNCTION__
-    #else
-                           __FUNCTION__
-    #endif
-                           + FRONT_SIZE, size - 1u);
-
+                    memcpy(typeName, __FUNCTION__ + FRONT_SIZE, size - 1u);
                     return typeName;
                 }
             };
         }
+#endif
 
 
         template<typename T>
         const char *GetTypeName(void) {
+#if defined(PAX_COMPILER_MSVC) || defined(PAX_COMPILER_CLANG)
             return internal::GetTypeNameHelper<T>::GetTypeName();
+#else
+            return typeid(T).name();
+#endif
         }
     }
 }
