f31adc1f79b6b2a46e599a4c8f21a7ac4f09de55
Paul Maximilian Bittner <p.bittner@tu-bs.de>
2018-04-19 20:19:48 +0200

    Fixed gitignore and commited missing LibIncludes.h

---
+++ include/utility/lib/LibIncludes.h
@@ -0,0 +1,13 @@
+//
+// Created by Paul on 29.04.2017.
+//
+
+#ifndef PAXENGINE3_LOGINCLUDES_H
+#define PAXENGINE3_LOGINCLUDES_H
+
+#include <easylogging++.h>
+
+// Initialize easylogging
+INITIALIZE_EASYLOGGINGPP
+
+#endif //PAXENGINE3_LOGINCLUDES_H
