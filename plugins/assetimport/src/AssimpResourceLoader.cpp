5c62d2c5cd96843921a9e16a1fd9d9eaaa10720b
Paul Maximilian Bittner <p.bittner@tu-bs.de>
2018-09-01 04:04:16 +0200

    begin of assimp loader for mingw (prototype CMake for MSVC)

--- plugins/assetimport/src/AssimpResourceLoader.cpp
+++ plugins/assetimport/src/AssimpResourceLoader.cpp
@@ -8,26 +8,48 @@
 
 #include <vector>
 
+/// assimp
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 namespace PAX {
     namespace AssetImport {
-        bool AssimpResourceLoader::canLoad(PAX::Path p) {
+        bool AssimpResourceLoader::canLoad(Path p) {
             static Util::FileTypeChecker formatChecker({
                 "3ds"
             });
             return formatChecker.check(p);
         }
 
-        std::shared_ptr<Mesh> AssimpResourceLoader::load(PAX::Path p) {
-            return Services::GetResources().load<Mesh>(
-                    std::vector<glm::vec3>({
-                        {0, 0, 0},
-                        {0, 1, 0},
-                        {1, 1, 0}
-                    }),
-                    std::vector<glm::ivec3>({
-                        {0, 1, 2}
-                    })
-            );
+        std::shared_ptr<Mesh> AssimpResourceLoader::load(Path p) {
+            Assimp::Importer importer;
+
+            // TODO: Find optimal default value and make this configurable
+            unsigned int postProcessingFlags = aiProcessPreset_TargetRealtime_Fast;
+            const aiScene* scene = importer.ReadFile(p.toString(), postProcessingFlags);
+
+            if (!scene) {
+                // Import failed
+                std::stringstream errorStream;
+                errorStream << "[PAX::AssetImport::AssimpResourceLoader::load] " << importer.GetErrorString();
+                throw std::runtime_error(errorStream.str());
+            }
+
+            // Now traverse the imported scene
+
+            // sample test code
+            std::vector<glm::vec3> vertices({
+                                           {0, 0, 0},
+                                           {0, 1, 0},
+                                           {1, 1, 0}
+                                   });
+            std::vector<glm::ivec3> faces({
+                                            {0, 1, 2}
+                                    });
+
+            std::shared_ptr<Mesh> m = Services::GetFactory().create<Mesh>(&vertices, &faces);
+            return m;
         }
     }
 }
