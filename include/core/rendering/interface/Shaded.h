efab2e4d0897d51f4f5469972e68937fe8d23ffc
Paul Maximilian Bittner <p.bittner@tu-bs.de>
2018-09-04 20:56:52 +0200

    added missing file

---
+++ include/core/rendering/interface/Shaded.h
@@ -0,0 +1,20 @@
+//
+// Created by Paul on 09.01.2018.
+//
+
+#ifndef PAXENGINE3_SHADED_H
+#define PAXENGINE3_SHADED_H
+
+#include <memory>
+
+#include <core/rendering/data/Shader.h>
+
+namespace PAX {
+    class Shaded {
+    public:
+        virtual void registerFlags(Shader::Flags &flags) {};
+        virtual void cacheUniformsFor(std::shared_ptr<Shader> &shader) {};
+    };
+}
+
+#endif //PAXENGINE3_SHADED_H
