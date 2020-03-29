1ba38cf43af4413e4edee27a346b2158c99a06ca
Bittner <paul.bittner@uni-ulm.de>
2020-03-29 17:03:25 +0200

    fixed wrong cmake configurations and removed deprecated architecture detection

--- cmake/PaxArchitectureDetection.cmake
+++ cmake/PaxArchitectureDetection.cmake
@@ -1,15 +1,14 @@
-# Requires: Set variable CMAKE_MODULE_PATH
 # Defines: PAX_OS_ARCHITECTURE
 
-#include(${CMAKE_MODULE_PATH}/TargetArch.cmake)
-set(PAX_OS_ARCHITECTURE x64) # This is the default value
+set(PAX_OS_ARCHITECTURE x86) # This is the default value
+
 if (CMAKE_SIZEOF_VOID_P EQUAL 8)
     set(PAX_OS_ARCHITECTURE x64)
 elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
     set(PAX_OS_ARCHITECTURE x86)
+else()
+    message(WARNING "Unknown Architecture: CMAKE_SIZEOF_VOID_P is ${CMAKE_SIZEOF_VOID_P}")
+    message(WARNING "Continuing with ${PAX_OS_ARCHITECTURE}")
 endif()
 
-
-#target_architecture(PAX_OS_ARCHITECTURE)
-#simplifyArchitecture(PAX_OS_ARCHITECTURE)
 message("PAX_OS_ARCHITECTURE is ${PAX_OS_ARCHITECTURE}")
--- cmake/PaxFlagsDefinition.cmake
+++ cmake/PaxFlagsDefinition.cmake
@@ -14,21 +14,21 @@ if (WIN32)
         message("    and MSVC")
 
         set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} /std:c++${PAX_CXX_STANDARD}")
-        set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
-        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
-
-        # Replace each occurence of /MD with /MT for static linking an avoid warnings for multiple definition
-        set(CompilerFlags
-                CMAKE_CXX_FLAGS
-                CMAKE_CXX_FLAGS_DEBUG
-                CMAKE_CXX_FLAGS_RELEASE
-                CMAKE_C_FLAGS
-                CMAKE_C_FLAGS_DEBUG
-                CMAKE_C_FLAGS_RELEASE
-                )
-        foreach(CompilerFlag ${CompilerFlags})
-            string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
-        endforeach()
+#        set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
+#        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
+#
+#        # Replace each occurence of /MD with /MT for static linking an avoid warnings for multiple definition
+#        set(CompilerFlags
+#                CMAKE_CXX_FLAGS
+#                CMAKE_CXX_FLAGS_DEBUG
+#                CMAKE_CXX_FLAGS_RELEASE
+#                CMAKE_C_FLAGS
+#                CMAKE_C_FLAGS_DEBUG
+#                CMAKE_C_FLAGS_RELEASE
+#                )
+#        foreach(CompilerFlag ${CompilerFlags})
+#            string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
+#        endforeach()
     else(${PAX_COMPILER_IS_MSVC}) # condition for nested if
         message("    but not MSVC")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -std=c++${PAX_CXX_STANDARD}")# "-static-libgcc -static-libstdc++") #this may be wrong for unix
