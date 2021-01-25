af944524f19f0fefe0bac0c2dec292f91fb83a73
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-01-25 18:53:30 +0100

    refactored CMakeLists.txt to reuse cmake/PaxSetup.cmake

--- cmake/PaxFlagsDefinition.cmake
+++ cmake/PaxFlagsDefinition.cmake
@@ -1,18 +1,18 @@
 # Defines:
 #
-# PAX_CXX_STANDARD
+# PAX_CXX_STANDARD_MINIMUM
 # CMAKE_CXX_FLAGS
 # CMAKE_CXX_FLAGS_DEBUG
 # CMAKE_CXX_FLAGS_RELEASE
 
-set(PAX_CXX_STANDARD 17)
-#add_definitions(-DPAX_CXX_STANDARD=${PAX_CXX_STANDARD})
+set(PAX_CXX_STANDARD_MINIMUM 17)
+#add_definitions(-DPAX_CXX_STANDARD=${PAX_CXX_STANDARD_MINIMUM})
 
 if (WIN32)
     message("Set flags for WIN32")
     if (${PAX_COMPILER_IS_MSVC})
         message("    and MSVC")
-        set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} /std:c++${PAX_CXX_STANDARD} /Zc:__cplusplus")
+        set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} /std:c++${PAX_CXX_STANDARD_MINIMUM} /Zc:__cplusplus")
 #        set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
 #        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
 #
@@ -30,11 +30,11 @@ if (WIN32)
 #        endforeach()
     else(${PAX_COMPILER_IS_MSVC}) # condition for nested if
         message("    but not MSVC")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -std=c++${PAX_CXX_STANDARD}")# "-static-libgcc -static-libstdc++") #this may be wrong for unix
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -std=c++${PAX_CXX_STANDARD_MINIMUM}")# "-static-libgcc -static-libstdc++") #this may be wrong for unix
     endif(${PAX_COMPILER_IS_MSVC}) # condition for nested if
 elseif(UNIX)
     message("Set flags for UNIX")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++${PAX_CXX_STANDARD}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++${PAX_CXX_STANDARD_MINIMUM}")
     set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=address")
 else(WIN32) # condition for nested if
     message(WARNING "Platform not specified. Correct behaviour not guaranteed.")
