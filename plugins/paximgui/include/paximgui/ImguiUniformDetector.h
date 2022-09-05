f45574e950c22f2c6267fd79e3c9a4284ded2e64
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2022-09-05 16:38:23 +0200

    fix: better syntax error reporting in ImguiUniformDetector

--- plugins/paximgui/include/paximgui/ImguiUniformDetector.h
+++ plugins/paximgui/include/paximgui/ImguiUniformDetector.h
@@ -9,15 +9,23 @@
 #include "polypropylene/event/EventHandler.h"
 
 #include "Variable.h"
+#include "BaseErrorListener.h"
 
 namespace PAX {
     namespace Imgui {
-        class ImguiUniformDetector : public OpenGL::GLSLPreprocessor {
+        class ImguiUniformDetector : public OpenGL::GLSLPreprocessor, public antlr4::BaseErrorListener {
+            const std::string * currentCode = nullptr;
+
         public:
             EventHandler<Variables&> OnUniformsDetected;
 
+            ImguiUniformDetector();
             ~ImguiUniformDetector() override;
             std::string preprocess(const Path & codeFile, const std::string& code) override;
+
+            void syntaxError(antlr4::Recognizer *recognizer, antlr4::Token * offendingSymbol,
+                             size_t line, size_t charPositionInLine,
+                             const std::string &msg, std::exception_ptr e) override;
         };
     }
 }
--- plugins/paximgui/src/ImguiUniformDetector.cpp
+++ plugins/paximgui/src/ImguiUniformDetector.cpp
@@ -7,22 +7,47 @@
 #include "ImguiPPLexer.h"
 #include "ImguiPPParser.h"
 #include "paximgui/ImguiUniformDetectorVisitor.h"
+#include "paxutil/io/PrettyPrint.h"
 
 namespace PAX {
     namespace Imgui {
+        ImguiUniformDetector::ImguiUniformDetector() = default;
+
         ImguiUniformDetector::~ImguiUniformDetector() = default;
 
         std::string ImguiUniformDetector::preprocess(const PAX::Path &codeFile, const std::string &code) {
+            currentCode = &code;
+
             antlr4::ANTLRInputStream input(code);
             Antlr::ImguiPPLexer lexer(&input);
             antlr4::CommonTokenStream tokens(&lexer);
             Antlr::ImguiPPParser parser(&tokens);
+            parser.addErrorListener(this);
 
             ImguiUniformDetectorVisitor visitor;
             visitor.visitProgram(parser.program());
             OnUniformsDetected(visitor.variables);
 
+            currentCode = nullptr;
+
             return code;
+        }
+
+        void ImguiUniformDetector::syntaxError(
+                antlr4::Recognizer *recognizer,
+                antlr4::Token *offendingSymbol,
+                size_t line,
+                size_t charPositionInLine,
+                const std::string &msg,
+                std::exception_ptr e) {
+            std::stringstream output;
+            output << "Parsing Error at line " << line << " at character " << charPositionInLine << ": " << msg << std::endl;
+            if (currentCode) {
+                output << "Full Shader Code:\n------------------\n";
+                Util::printWithLineNumbersTo(output, *currentCode);
+                output << "==================";
+            }
+            PAX_LOG(Log::Level::Error, output.str());
         }
     }
 }
